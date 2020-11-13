ff26e9de9abeea53f4c54ccf9590bb978f8377c8
Federico Coppo <f.coppo@tecnosystem.it>
2020-11-13 14:05:30 +0100

    main starting routine.

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+	File        : main.cpp
+	Date        : 13/11/2020
+	Author      : Federico Coppo
+	Description : Main routine
+*******************************************************************************/
+
+// include
+
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,15 @@
+/*******************************************************************************
+	File        : main.h
+	Date        : 13/11/2020
+	Author      : Federico Coppo
+	Description : Main header
+*******************************************************************************/
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+// Run configuration
+#define TEST_APPLICATION   // switch to test
+
+
+#endif /* MAIN_H_ */
